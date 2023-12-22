101cd881fc0fb8381bc1c630763aceea2cd692d4
57qy654 <Jude.Pitschka@gmail.com>
2023-12-22 17:36:17 -0600

    Added welcome window

--- main.py
+++ main.py
@@ -1,4 +1,70 @@
+# Import the pygame module
+import pygame
+
+# Import pygame.locals for easier access to key coordinates
+# Updated to conform to flake8 and black standards
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+# Initialize pygame
+pygame.init()
+
+# Define constants for the screen width and height
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+# Create the screen object
+# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Apple Trivia Quiz")
+
+font = pygame.font.Font(None, 36)
+
+def draw_text(text, x, y, color=BLACK):
+    text_surface = font.render(text, True, color)
+    screen.blit(text_surface, (x, y))
+
+def main():
+    # Main loop
+
+    # Variable to keep the main loop running
+    running = True
+    while running:
+        # Look at every event in the queue
+        for event in pygame.event.get():
+            # Did the user hit a key?
+            if event.type == KEYDOWN:
+                # Was it the Escape key? If so, stop the loop.
+                if event.key == K_ESCAPE:
+                    running = False
+
+            # Did the user click the window close button? If so, stop the loop.
+            elif event.type == QUIT:
+                running = False
+
+        # Clear the screen
+        screen.fill(WHITE)
+
+        # Draw your game elements here
+        draw_text("Welcome to Apple Trivia!", 200, 100, RED)
+
+        # Update the display
+        pygame.display.flip()
+
+
 
 if __name__ == '__main__':
-    print_hi('PyCharm budder')
+    main()
 
