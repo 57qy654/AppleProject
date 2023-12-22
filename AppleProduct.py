ca7afb118fc544843bf52e8cb424a1c0b186a950
57qy654 <Jude.Pitschka@gmail.com>
2023-12-22 14:29:40 -0600

    Made AppleProduct class constructor.

---
+++ AppleProduct.py
@@ -0,0 +1,11 @@
+class AppleProduct:
+    def __innit__(self, name, product_type, color, size, year, operating_system, physical_keyboard):
+        self.name = name
+        self.product_type = product_type
+        self.color = color
+        self.size = size
+        self.year = year
+        self.operating_system = operating_system
+        self.physical_keyboard = physical_keyboard
+
+
---
+++ iMac.py
@@ -0,0 +1,2 @@
+class iMac(AppleProduct):
+    def __innit__(self, apple_silicon, pro=False, ):
