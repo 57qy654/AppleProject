c1a3d4796ef68b576d7d34caf11e1ed6065673c8
57qy654 <Jude.Pitschka@gmail.com>
2023-12-29 21:59:58 -0600

    Better organized code. Added logic for main menu and game states.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import the pygame module
 import pygame
+
 from Button import Button
 
 # Import pygame.locals for easier access to key coordinates
@@ -12,6 +13,7 @@ from pygame.locals import (
     K_ESCAPE,
     KEYDOWN,
     QUIT,
+    MOUSEBUTTONDOWN,
 )
 
 # Initialize pygame
@@ -26,6 +28,11 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
+# Game States
+MAIN_MENU = 0
+GAMEPLAY = 1
+
+
 # Create the screen object
 # The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -33,6 +40,7 @@ pygame.display.set_caption("Apple Trivia Quiz")
 pygame.display.set_caption("Button Example")
 
 font = pygame.font.Font(None, 36)
+
 
 def draw_text(text, x, y, color=BLACK):
     text_surface = font.render(text, True, color)
@@ -50,17 +58,72 @@ def calculate_center(width, height):
     center_y = (SCREEN_HEIGHT - height) / 2
     return center_x, center_y
 
+###################################
+#Button
+
+
+all_sprites = pygame.sprite.Group()
+
+button_width = 200
+button_height = 50
+
+quit_button = Button(*calculate_center(button_width, button_height - 150), button_width, button_height, "Quit", quit_game)
+start_button = Button(*calculate_center(button_width, button_height), button_width, button_height, "Start", start_game)
+
+all_sprites.add(quit_button, start_button)
+
+#####################################
+
+def handle_main_menu_events(event):
+    # Handle events for the main menu state
+    if event.type == MOUSEBUTTONDOWN:
+        if start_button.rect.collidepoint(event.pos):
+            # Transition to the gameplay state when the "Start" button is clicked
+            return GAMEPLAY
+
+    return MAIN_MENU  # Return the current state if no state change
+
+def update_main_menu():
+    # Update logic for the main menu state
+    pass
+
+def draw_main_menu():
+    # Clear the screen
+    screen.fill(WHITE)
+
+    # Draw elements for the main menu state
+    draw_text("Welcome to Apple Trivia!", 200, 100, RED)
+
+    # Draw all sprites
+    all_sprites.draw(screen)
+
+    # Update the display
+    pygame.display.flip()
+
+def handle_gameplay_events(event):
+    # Handle events for the gameplay state
+    if event.type == KEYDOWN:
+        if event.key == pygame.K_ESCAPE:
+            # Transition back to the main menu when ESC is pressed
+            return MAIN_MENU
+
+    return GAMEPLAY  # Return the current state if no state change
+    pass
+
+def update_gameplay():
+    # Update logic for the gameplay state
+    pass
+
+def draw_gameplay():
+    # Draw elements for the gameplay state
+    pass
+
 def main():
-    # Create sprite groups
-    all_sprites = pygame.sprite.Group()
 
-    button_width = 200
-    button_height = 50
 
-    quit_button = Button(*calculate_center(button_width, button_height - 150), button_width, button_height, "Quit", quit_game)
-    start_button = Button(*calculate_center(button_width, button_height), button_width, button_height, "Start", start_game)
+    # Initialize game state
+    current_state = MAIN_MENU
 
-    all_sprites.add(quit_button, start_button)
 
     # Main loop
 
@@ -74,22 +137,22 @@ def main():
                     running = False
             elif event.type == QUIT:
                 running = False
-            else:
+            elif current_state == MAIN_MENU:
+                current_state = handle_main_menu_events(event)
+            elif current_state == GAMEPLAY:
+                handle_gameplay_events(event)
+
+            # Update and draw based on the current state
+            if current_state == MAIN_MENU:
+                update_main_menu()
+                draw_main_menu()
                 # Pass the event to each sprite individually
                 for sprite in all_sprites:
                     sprite.handle_event(event)
+            elif current_state == GAMEPLAY:
+                update_gameplay()
+                draw_gameplay()
 
-        # Update all sprites
-        all_sprites.update()
-
-        # Clear the screen
-        screen.fill(WHITE)
-
-        # Draw your game elements here
-        draw_text("Welcome to Apple Trivia!", 200, 100, RED)
-
-        # Draw all sprites
-        all_sprites.draw(screen)
 
 
 
